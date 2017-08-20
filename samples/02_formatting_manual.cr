66c79852eb1b6bef52486e8c9dd42f8a8ea0d921
Benoit de Chezelles <lesell_b@epitech.eu>
2017-08-20 18:53:59 +0200

    Add manual formatting sample, using raw formatting helpers

---
+++ samples/02_formatting_manual.cr
@@ -0,0 +1,47 @@
+#!/usr/bin/env crystal
+
+require "../src/lemonade"
+require "../src/lemonade/formatting_helper"
+
+include Lemonade
+H = FormattingHelper
+
+lemon = Lemon.new
+lemon.underline_height = 3
+
+bar = Bar.new
+
+content = Block::TextBlock.new "content"
+
+bar.center.separator = Block::TextSpacerBlock.new 3
+bar.center << content
+
+# bar.center << H.fg content, Material::Teal_500
+container = Block::Container.new
+container << H.fg_color(Material::Teal_500)
+container << content
+container << H.fg_reset
+bar.center << container
+
+# bar.center << H.ul H.bg content, Material::Red_500
+container = Block::Container.new
+container << H.enable_underline << H.bg_color(Material::Red_500)
+container << content
+container << H.bg_reset << H.disable_underline
+bar.center << container
+
+# bar.center << H.fgbg content, fg: Material::Blue_500, bg: Material::White
+container = Block::Container.new
+container << H.fg_color(Material::Blue_500) << H.bg_color(Material::White)
+container << content
+container << H.bg_reset << H.fg_reset
+bar.center << container
+
+# bar.center << H.ul content, Material::Red_500
+container = Block::Container.new
+container << H.enable_underline << H.line_color(Material::Red_500)
+container << content
+container << H.line_reset << H.disable_underline
+bar.center << container
+
+lemon.run bar, interval: 1.second

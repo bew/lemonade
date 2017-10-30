ab21a1ec87fe5795e603597a3c58ed5087785ce0
Benoit de Chezelles <lesell_b@epitech.eu>
2017-10-30 10:30:37 +0100

    Start LemonManager skeleton

--- src/lemonade.cr
+++ src/lemonade.cr
@@ -2,9 +2,46 @@ require "./lemonade/**"
 require "./material_color"
 
 module Lemonade
-  def self.new
-    Lemon.new
+  @@manager = LemonManager.new
+
+  def self.current_manager
+    @@manager
+  end
+
+  def manage(lemon)
+    current_manager.manage(lemon)
+  end
+
+  def wait(*lemons)
+    current_manager.wait(lemons)
   end
 end
 
+class Lemonade::LemonManager
+  enum Event
+    Exited
+  end
 
+  class LemonData
+    getter event_notifier = Channel(Event).new
+  end
+
+  getter managed_lemons = {} of Lemon => LemonData
+
+  def manage(lemon)
+    return if managed_lemons.includes(lemon)
+
+    setup_lemon_process(lemon)
+    managed_lemons << lemon
+  end
+
+  def wait(lemons)
+    event_notifiers = managed_lemons.map do |(lemon, data)|
+      if lemons.includes?(lemon)
+        return data.event_notifier
+      end
+    end.compact!
+
+    # wait for event_notifier of all lemons to send Exited event
+  end
+end
